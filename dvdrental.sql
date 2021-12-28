551242b3c2598f9f9d2ff8746ecb82be213044be
Chethan C K <45870776+ck-chethan@users.noreply.github.com>
2021-12-28 14:39:30 +0530

    Update dvdrental.sql

--- dvdrental.sql
+++ dvdrental.sql
@@ -319,3 +319,18 @@ SELECT CAST('5' AS INTEGER) AS new_int;
 SELECT '5' :: INTEGER AS new_int;
 
 SELECT CHAR_LENGTH(CAST(inventory_id AS VARCHAR)) FROM rental;
+
+CREATE VIEW customer_info AS
+SELECT first_name, last_name, address 
+FROM customer INNER JOIN address 
+ON customer.address_id = address.address_id;
+
+SELECT * FROM c_info;
+
+CREATE OR REPLACE VIEW customer_info AS
+SELECT first_name, last_name, address, district 
+FROM customer INNER JOIN address 
+ON customer.address_id = address.address_id;
+
+DROP VIEW IF EXISTS customer_info;
+ALTER VIEW customer_info RENAME TO c_info;
