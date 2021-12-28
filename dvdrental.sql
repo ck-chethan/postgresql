3c0856327d4ed9e6f53dcfe6e92aa688dffe9fa3
Chethan C K <45870776+ck-chethan@users.noreply.github.com>
2021-12-28 13:31:03 +0530

    Update dvdrental.sql

--- dvdrental.sql
+++ dvdrental.sql
@@ -313,3 +313,9 @@ SUM(CASE rating
 	ELSE 0
 END) AS pg13
 FROM film;
+
+
+SELECT CAST('5' AS INTEGER) AS new_int;
+SELECT '5' :: INTEGER AS new_int;
+
+SELECT CHAR_LENGTH(CAST(inventory_id AS VARCHAR)) FROM rental;
